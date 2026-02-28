d375cc2c83b18501ae7fa74c9610048c3e891489
Tinglyhubowneristouritobtw <tarifyoussef98@gmail.com>
2026-02-28 13:13:53 +0000

    Create tinglyhub2.lua

---
+++ tinglyhub2.lua
@@ -0,0 +1,9 @@
+-- Script 1
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/U6zFeKgn/raw"))()
+end)
+
+-- Script 2
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/134M2A5z/raw"))()
+end)
